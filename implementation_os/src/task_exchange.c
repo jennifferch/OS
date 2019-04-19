136021706e73bc230b56db3c7b645c6e9dc7f5f4
unknown <jenniffer.chavez@gmail.com>
2019-04-19 14:45:33 -0300

    .

--- implementation_os/src/task_exchange.c
+++ implementation_os/src/task_exchange.c
@@ -6,9 +6,12 @@ uint32_t exchangeStack[STACK_SIZE];
 
 void task_exchange(void *a) {
 
+	blinkTaskData_t *myConfig = (blinkTaskData_t *) a;
 	uint8_t ping = 1;
 
 	while (TRUE) {
+
+		gpioToggle(myConfig->led);
 
 		if (!ping) {
 			os_event_set(&processEvent);
